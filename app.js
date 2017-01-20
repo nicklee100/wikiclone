5ecc00c27c09b0edb7611b0a4524e3b7040d73c6
Nick Lee <nicklee100@gmail.com>
2017-01-20 17:15:50 -0600

    views and templating engine working

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+app.use(morgan('dev'));
 
+app.use(express.static('views'));
 
-app.listen(1337, function(){
-    console.log(' .... running on 1337' )
-});
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+app.listen(3000, () => console.log(' .... running on 3000' ));
